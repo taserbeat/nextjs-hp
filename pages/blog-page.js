edd852faa18b67451a943e84f29a542fadc819b0
taserbeat <taserbeat@gmail.com>
2021-08-22 15:26:22 +0900

    Blogページで記事のリストをSSGで生成

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
+import { getAllPostsData } from '../lib/posts';
+import Post from '../components/Post';
 
-const Blog = () => {
-  return <Layout title="Blog">Blog Page</Layout>;
+const Blog = ({ posts }) => {
+  return (
+    <Layout title="Blog">
+      <ul className="m-10">
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
+    </Layout>
+  );
 };
+
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+
+  return {
+    props: { posts },
+  };
+}
 
 export default Blog;
